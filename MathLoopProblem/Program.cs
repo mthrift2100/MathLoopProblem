ec4d34dfa8009c55ead85df53b526ed5dceeae9d
michaelthrift <michael.thrift@striqs.com>
2022-05-26 09:35:33 -0400

    Add project files.

---
+++ MathLoopProblem/Program.cs
@@ -0,0 +1,32 @@
+﻿using MathLoopProblem;
+
+
+bool keepGoing = true;
+
+while (keepGoing)
+{
+    Console.Write("Enter a whole integer value: ");
+    string? response = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(response))
+    {
+        Console.WriteLine("You must provide a valid whole number integer!");
+        continue;
+    }
+          
+    if (long.TryParse(response, out var value))
+    {
+
+        var helper = new MathHelper();
+        Console.WriteLine("Processing ....");
+        Console.WriteLine("");
+        int result = helper.RunEngine(value);
+        Console.WriteLine($"Number {value} took {result} iterations to enter the loop. The highest value reached was: {helper.HighestValue}");
+        Console.WriteLine("");
+    }
+    else
+    {
+        Console.WriteLine("You must provide a valid whole number integer!");
+        continue;
+    }
+}
